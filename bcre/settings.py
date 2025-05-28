5a46cde931954392c5e4b55b14168c0c1866571d
steve <steve.ct290ds@gmail.com>
2025-05-28 12:09:32 +0800

    Step 4: install listings and debug toolbar

--- bcre/settings.py
+++ bcre/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    "debug_toolbar",
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'bcre.urls'
@@ -126,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- bcre/urls.py
+++ bcre/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path('',include('pages.urls', namespace='pages')),
-    path('admin/', admin.site.urls),
-]
+    path ('', include('pages.urls', namespace='pages')),
+    path ('listings/', include('listings.urls', namespace='listings')),
+    path ('admin/', admin.site.urls),
+] + debug_toolbar_urls()
