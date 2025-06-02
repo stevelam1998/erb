6706b2a791f7300e216ef53786b4cd01378cf7c9
steve <steve.ct290ds@gmail.com>
2025-06-02 11:08:39 +0800

    Step 8: about page

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
-    pages_url = reverse('pages:index')
-    return render(request,'pages/index.html')
+#    pages_url = reverse('pages:index')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context ={"listings" : listings }
+    return render(request,'pages/index.html', context)
 def about(request):
-    pages_url = reverse('pages:about')    
-    return render(request,'pages/about.html')
+#    pages_url = reverse('pages:about')    
+    realtors_base = Realtor.objects.all()
+    realtors=Realtor.objects.order_by('-hire_date')
+#    mvp_realtors =Realtor.objects.all().filter(is_mvp=True)
+    mvp_realtors = realtors_base.filter(is_mvp=True)
+    context = {"realtors" : realtors, "mvp_realtors":mvp_realtors}
+    return render(request,'pages/about.html',context)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Realtor
 # Register your models here.
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'email','hire_date')
+    list_display = ('id','name', 'email','phone','is_mvp')
     list_display_links = ('id','name')
+    list_editable ='is_mvp','email', 'phone'
     search_fields = ('name',)
     list_per_page = 25
 
